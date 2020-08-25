ab8f3048b2262f7cf5d9ca9feb2cead96f778d8c
Hokuto Yamada <hokuto.yamada.j@gmail.com>
2020-08-25 13:49:30 +0900

    modify response

--- index.py
+++ index.py
@@ -4,5 +4,10 @@ def handler(event, context):
     # TODO implement
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda!')
+        'body': json.dumps({
+            'contents': [
+                'Hello from Lambda! 2',
+                'test'
+            ]
+        })
     }
